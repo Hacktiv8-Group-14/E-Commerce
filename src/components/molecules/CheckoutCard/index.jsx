b2da8f323cfb4cf8af2201d3fc852fd4ba913692
muchlisaryn <muchlisar68@gmail.com>
2022-10-30 17:57:32 +0700

    delete button in Checkoutcard

--- src/components/molecules/CheckoutCard/index.jsx
+++ src/components/molecules/CheckoutCard/index.jsx
@@ -10,8 +10,7 @@ export default function Checkout(props) {
         <div>Checkout</div>
         <Quantity stock={stock} price={price} />
         <div className="mt-5">
-          <Button className="w-full  bg-slate-500 p-2">Buy Now</Button>
-          <Button className="w-full mt-5 bg-slate-500 p-2">Keranjang</Button>
+          <Button className="w-full mt-5 bg-slate-500 p-2">+ Keranjang</Button>
         </div>
       </div>
     </>
