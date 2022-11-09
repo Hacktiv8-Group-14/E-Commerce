d09fc2cc227da08f5489b7f6fe9ccec1dbc25d0c
rereard <rereardany60@gmail.com>
2022-11-09 15:52:46 +0700

    add loading screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { routes } from "./Routing";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import ReactLoading from "react-loading"
 import { fetchProducts } from "./features/productSlice";
 import ScrollToTop from "./components/atoms/ScrollToTop";
 
 function App() {
   const dispatch = useDispatch();
+  const isPending = useSelector(state => state.products.isPending)
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -14,12 +16,21 @@ function App() {
 
   return (
     <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        {routes?.map((item, index) => (
-          <Route key={index} path={item.path} element={<item.element />} />
-        ))}
-      </Routes>
+      {isPending ? (
+        <div className="h-screen w-screen flex flex-col justify-center items-center gap-4">
+          <ReactLoading type="spin" color="#242582" height={100} width={100} />
+          <div className="text-2xl">Loading contents...</div>
+        </div>
+      ) : (
+        <>
+          <ScrollToTop />
+          <Routes>
+            {routes?.map((item, index) => (
+              <Route key={index} path={item.path} element={<item.element />} />
+            ))}
+          </Routes>
+        </>
+      )}
     </BrowserRouter>
   );
 }
